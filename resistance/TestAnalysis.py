198963156d7cc9f2867806f251619b969916802b
JakeL1001 <jakelyell@iinet.net.au>
2021-10-26 10:27:35 +0800

    Commented TestAnalysis.py

--- resistance/TestAnalysis.py
+++ resistance/TestAnalysis.py
@@ -1,32 +1,32 @@
 import pandas as pd
 
 if __name__ == "__main__":
-    files = ["BasicBayesBasic.csv", "BasicBayesCombination.csv", "BasicBayesJond.csv", "BasicBayesRandom.csv", 
+    files = ["BasicBayesBasic.csv", "BasicBayesCombination.csv", "BasicBayesJond.csv", "BasicBayesRandom.csv",  # List of files to analyse
              "BayesJondBasic.csv", "BayesJondCombination.csv", "BayesJondJond.csv", "BayesJondRandom.csv"]
-    # files = ["RandomAgentBasic.csv", "RandomAgentCombination.csv", "RandomAgentJond.csv", "RandomAgentRandom.csv",]
-    for x in files:
-        print(x)
-        df = pd.read_csv(x)
-        df = df.reset_index(drop=True)
-        # print(df)
+    # files = ["RandomAgentBasic.csv", "RandomAgentCombination.csv", "RandomAgentJond.csv", "RandomAgentRandom.csv",] # Analyses the RandomAgent files if tested
+    
+    for x in files: # For all of the files in the list
+        # print(x)
+        df = pd.read_csv(x) # Reads the file
+        df = df.reset_index(drop=True) # Resets the index of dataframe
 
         df.columns = ['Players','Role', 'Win']
-        total_wins = df[df['Win'] == 'Won']
-        spy_wins = df[df['Role'] == 'I was spy' ]
-        games_as_spy = spy_wins['Win'].count()
-        spy_wins = spy_wins[spy_wins['Win'] == 'Won']
+        total_wins = df[df['Win'] == 'Won'] # creates a dataframe containing only wins
+        spy_wins = df[df['Role'] == 'I was spy' ] # creates a dataframe containing only spy games
+        games_as_spy = spy_wins['Win'].count() # Counts the number of games as spy
+        spy_wins = spy_wins[spy_wins['Win'] == 'Won'] # creates a dataframe containing only spy wins
 
-        res_wins = df[df['Role'] == 'I was not spy' ]
-        games_as_res = res_wins['Win'].count()
-        res_wins = res_wins[res_wins['Win'] == 'Won']
+        res_wins = df[df['Role'] == 'I was not spy' ] # creates a dataframe containing only resistance games
+        games_as_res = res_wins['Win'].count() # Counts the number of games as resistance
+        res_wins = res_wins[res_wins['Win'] == 'Won'] # creates a dataframe containing only resistance wins
 
-        wins = total_wins['Win'].count()
-        total_winrate =  (spy_wins['Win'].count() + res_wins['Win'].count()) / (games_as_res + games_as_spy)
-        spy_winrate = spy_wins['Win'].count()/games_as_spy
-        res_winrate = res_wins['Win'].count()/games_as_res
-        print('Total winrate was:', total_winrate)
-        print('Spy winrate was:', spy_winrate)
-        print("Games as spy =", games_as_spy)
-        print('Resistance winrate was:', res_winrate)
-        print("Games as Resistance =", games_as_res)
+        wins = total_wins['Win'].count() # Counts the number of wins
+        total_winrate =  (spy_wins['Win'].count() + res_wins['Win'].count()) / (games_as_res + games_as_spy) # Calculates the winrate
+        spy_winrate = spy_wins['Win'].count()/games_as_spy # Calculates the winrate of spy games
+        res_winrate = res_wins['Win'].count()/games_as_res # Calculates the winrate of resistance games
+        print('Total winrate was:', total_winrate) # Prints the winrate
+        print('Spy winrate was:', spy_winrate) # Prints the spy winrate
+        print("Games as spy =", games_as_spy) # Prints the number of games as spy
+        print('Resistance winrate was:', res_winrate) # Prints the resistance winrate
+        print("Games as Resistance =", games_as_res) # Prints the number of games as resistance
         print()
