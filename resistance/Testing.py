b2850ca041ca56c2a7b116d1307a9e0d14c23868
JakeL1001 <jakelyell@iinet.net.au>
2021-10-26 10:22:25 +0800

    Edited Testing File

--- resistance/Testing.py
+++ resistance/Testing.py
@@ -19,7 +19,7 @@ def loopgames(subject, opponentstring, x, opponents, games):
 
 def loopgames2(opponentstringtolist, opponentstring, games, subject):
     opponents = opponentstringtolist[opponentstring]
-    # subject = subject + "Agent" # ONLY NEEDED FOR TESTING THE RANDOM AGENT
+    # subject = subject + "Agent" # ONLY NEEDED FOR TESTING THE RANDOM AGENT uncomment when testing random agent
     for x in range(5,11): # Tests game size between 5 and 10 inclusive
         print("Testing " + subject + " against " + opponentstring + " with " + str(x) + " agents")
         if opponentstring == "Combination": # Randomizes the combination of agents
@@ -31,7 +31,7 @@ def loopgames2(opponentstringtolist, opponentstring, games, subject):
             game.play()
 
 if __name__ == "__main__":
-    Basic = [BasicBayes(name='basic1'),
+    Basic = [BasicBayes(name='basic1'), # Creates team of BasicBayes opposing agents, up to 9 players, creating a team of 10
             BasicBayes(name='basic2'),  
             BasicBayes(name='basic3'),    
             BasicBayes(name='basic4'),  
@@ -41,7 +41,7 @@ if __name__ == "__main__":
             BasicBayes(name="basic8"),
             BasicBayes(name="basic9")]
     
-    Jond = [BayesJond(name='jond1'),
+    Jond = [BayesJond(name='jond1'), # Creates team of BayesJond opposing agents, up to 9 players, creating a team of 10
             BayesJond(name='jond2'),
             BayesJond(name='jond3'),
             BayesJond(name='jond4'),
@@ -51,7 +51,7 @@ if __name__ == "__main__":
             BayesJond(name="jond8"),
             BayesJond(name="jond9")]
     
-    Random = [RandomAgent(name='random1'),
+    Random = [RandomAgent(name='random1'), # Creates team of RandomAgent opposing agents, up to 9 players, creating a team of 10
             RandomAgent(name='random2'),
             RandomAgent(name='random3'),
             RandomAgent(name='random4'),
@@ -61,7 +61,7 @@ if __name__ == "__main__":
             RandomAgent(name="random8"),
             RandomAgent(name="random9")]
     
-    Combination = [RandomAgent(name='random1'),
+    Combination = [RandomAgent(name='random1'), # Creates team of a combination of the other opposing agents, up to 9 players, creating a team of 10
                 BasicBayes(name='basic1'),
                 BayesJond(name='jond1'),
                 RandomAgent(name='random2'),
@@ -71,64 +71,23 @@ if __name__ == "__main__":
                 BasicBayes(name='basic3'),
                 BayesJond(name='jond3'),]
         
-    agentsToTest = ["BasicBayes", "BayesJond"]
-    # agentsToTest = ["Random"]
-    opponentlist = [Random, Basic, Jond, Combination]
-    # opponentlisttostring = {Random: "Random", Basic: "Basic", Jond: "Jond", Combination: "Combination"}
-    opponentstringtolist = {'Random': Random, 'Basic': Basic, 'Jond': Jond, 'Combination': Combination}
+    agentsToTest = ["BasicBayes", "BayesJond"] # List of agents to test
+    # agentsToTest = ["Random"] # Test separately to compare results
+    opponentlist = [Random, Basic, Jond, Combination] # List of teams to test each agent against
+    opponentstringtolist = {'Random': Random, 'Basic': Basic, 'Jond': Jond, 'Combination': Combination} 
     
-    games = 20000
+    games = 20000   # Number of games for each agent to verse each type of team for each size of game. eg for 2 agents vs 4 teams with between 5-10 players and 20,000 games, 
+                    # there will be a total of 960,000 test games played
     
-    # for subject in agentsToTest: #Tests BasicBayes and BayesJond
-    #     for opponentstring in opponentstringtolist: # Tests against Random, Basic, Jond, and Combination
-    #         opponents = opponentstringtolist[opponentstring]
-    #         for x in range(5,11): # Tests game size between 5 and 10 inclusive
-    #             print("Testing " + subject + " against " + opponentstring + " with " + str(x) + " agents")
-    #             if opponentstring == "Combination": # Randomizes the combination of agents
-    #                 random.shuffle(opponents)
-    #             agents = opponents[:x-1]
-    #             agents.append(eval(subject)(name=subject + opponentstring)) # Adds the subject to the agents list
-    #             for i in range(games): # Tests each combination 20,000 times
-    #                 game = Game(agents)
-    #                 game.play()
-
     for subject in agentsToTest: #Tests BasicBayes and BayesJond
         processes = []
         for opponentstring in opponentstringtolist: # Tests against Random, Basic, Jond, and Combination
-            p = Process(target=loopgames2, args=(opponentstringtolist, opponentstring, games, subject))
+            p = Process(target=loopgames2, args=(opponentstringtolist, opponentstring, games, subject)) # Uses multiprocessing to run much faster, each thread tests an opposing team
             processes.append(p)
             p.start()
         for p in processes:
             p.join()
-
-# def loopgames2(opponentstringtolist, opponentstring, games, subject):
-        # processes = []
-        # for i in range(5,11): # Runs 6 processes, each testing a different number of agents (DIFFERENT NUMBER OF PLAYERS NOT YET IMPLEMENTED)
-        #         print(i)
-        #         p = Process(target=loopgames, args=(NO_GAMES,))
-        #         processes.append(p)
-        #         p.start()
-        # for p in processes:
-        #         p.join()
-
-
-        # df = pd.read_csv('outcomes.csv')
-        # df = df.reset_index()
-
-        # df.columns = ['Role', 'Win']
-        # total_wins = df[df['Win'] == 'Won']
-        # spy_wins = df[df['Role'] == 'I was spy' ]
-        # games_as_spy = spy_wins['Win'].count()
-        # spy_wins = spy_wins[spy_wins['Win'] == 'Won']
-
-        # res_wins = df[df['Role'] == 'I was not spy' ]
-        # games_as_res = res_wins['Win'].count()
-        # res_wins = res_wins[res_wins['Win'] == 'Won']
-
-        # wins = total_wins['Win'].count()
-        # total_winrate =  (spy_wins['Win'].count() + res_wins['Win'].count()) / (games_as_res + games_as_spy)
-        # spy_winrate = spy_wins['Win'].count()/games_as_spy
-        # res_winrate = res_wins['Win'].count()/games_as_res
-        # print('Total winrate was:', total_winrate)
-        # print('Spy winrate was:', spy_winrate)
-        # print('Resistance winrate was:', res_winrate)
+            
+    '''
+    REMEMBER, BEFORE RUNNING, UNCOMMMENT THE LAST FUNCITON IN THE EACH OF THE BASICBAYES AND BAYESJOND AGENTS, OR NO CSV OF RESULTS WILL BE MADE
+    '''
--- resistance/__main__.py
+++ resistance/__main__.py
@@ -5,107 +5,18 @@ from BayesJond import BayesJond
 from game import Game
 import random
 import pandas as pd
-from multiprocessing import Process
-
-
-# agents = [RandomAgent(name='r1'), 
-#         RandomAgent(name='r2'),  
-#         RandomAgent(name='r3'),  
-#         # RandomAgent(name='r4'),  
-#         RandomAgent(name='r5'),  
-#         RandomAgent(name='r6'),  
-#         RandomAgent(name='r7'),
-#         Agent1(name="TEST")]
-
-
-
-# agents = [Agent1(name='TEST'), 
-#         Agent2(name='r2'),  
-#         RandomAgent(name='r3'),  
-#         # RandomAgent(name='r4'),  
-#         RandomAgent(name='r5'),  
-#         RandomAgent(name='r6'),  
-#         RandomAgent(name='r7'),
-#         BayesJond(name="r8")]
-
-
-# agents = [BayesJond(name='r1'), 
-#         BayesJond(name='r2'),  
-#         BayesJond(name='r3'),  
-#         # RandomAgent(name='r4'),  
-#         BayesJond(name='r5'),  
-#         BayesJond(name='r6'),  
-#         BayesJond(name='r7'),
-#         BayesJond(name="TEST")]
-
-
-# agents = [Agent1(name='r1'), 
-#         Agent1(name='r2'),  
-#         Agent1(name='r3'),  
-#         # RandomAgent(name='r4'),  
-#         Agent1(name='r5'),  
-#         Agent1(name='r6'),  
-#         BasicBayes(name='r7'),
-#         BasicBayes(name="TEST")]
-
-# agents = [Agent2(name='r1'), 
-#         Agent2(name='r2'),  
-#         Agent2(name='r3'),  
-#         # RandomAgent(name='r4'),  
-#         Agent2(name='r5'),  
-#         Agent2(name='r6'),  
-#         Agent2(name='r7'),
-#         Agent2(name="TEST")]        
-
-# game = Game(agents)
-# game.play()
-# print(game.missions_lost)
-agents = [RandomAgent(name='r1'), 
-                RandomAgent(name='r2'),  
-                RandomAgent(name='r3'),  
-                # RandomAgent(name='r4'),  
-                RandomAgent(name='r5'),  
-                RandomAgent(name='r6'),  
-                RandomAgent(name='r7'),
-                BasicBayes(name="TEST")]
-
-def loopgames(NO_GAMES):
-                print("looping")
                 
 if __name__ == "__main__":
 
 
         df = pd.DataFrame(list())
-        df.to_csv('outcomes.csv')
 
         NO_GAMES = 100
-
-        # processes = []
-        # for i in range(5,11): # Runs 6 processes, each testing a different number of agents (DIFFERENT NUMBER OF PLAYERS NOT YET IMPLEMENTED)
-        #         print(i)
-        #         p = Process(target=loopgames, args=(NO_GAMES,))
-        #         processes.append(p)
-        #         p.start()
-        # for p in processes:
-        #         p.join()
-
-        # for p in range(7,8):
-        #         print(p)
-        #         agents = []
-        #         for x in range(1,p):
-        #                 agents.append(RandomAgent(name="r{}".format(str(x))))
-        #         agents.append(BayesJond(name="TEST"))
-        #         random.shuffle(agents)
-        #         print(agents)
-        #         for i in range(NO_GAMES):
-        #                 game = Game(agents)
-        #                 game.play()
 
         
         agents1 = [BasicBayes(name='r1'), 
                                 BasicBayes(name='r2'),  
-                                BasicBayes(name='r3'),  
-                                # RandomAgent(name='r4'),  
+                                BasicBayes(name='r3'),   
                                 BasicBayes(name='r4'),  
                                 BasicBayes(name='r5'),  
                                 BasicBayes(name='r6'),
@@ -115,7 +26,6 @@ if __name__ == "__main__":
         agents2 = [Agent2(name='r1'),
                                 Agent2(name='r2'),
                                 Agent2(name='r3'),
-                                # RandomAgent(name='r4'),
                                 Agent2(name='r4'),
                                 Agent2(name='r5'),
                                 Agent2(name='r6'),
@@ -125,7 +35,6 @@ if __name__ == "__main__":
         agentsr = [RandomAgent(name='r1'),
                                 RandomAgent(name='r2'),
                                 RandomAgent(name='r3'),
-                                # RandomAgent(name='r4'),
                                 RandomAgent(name='r4'),
                                 RandomAgent(name='r5'),
                                 RandomAgent(name='r6'),
@@ -146,21 +55,6 @@ if __name__ == "__main__":
                                 game = Game(agents)
                                 game.play()
                 
-                
-        # agents = [RandomAgent(name='r1'), 
-        #         RandomAgent(name='r2'),  
-        #         RandomAgent(name='r3'),  
-        #         RandomAgent(name='r4'),
-        #         BasicBayes(name="TEST")]
-
-        # for i in range(NO_GAMES):
-        #         # print(i)
-        #         game = Game(agents)
-        #         game.play()
-
-
-
-        df = pd.read_csv('outcomes.csv')
         df = df.reset_index()
 
         df.columns = ['Role', 'Win']
